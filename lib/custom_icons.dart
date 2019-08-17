0d5adb4a5dbc809e2d002f88ca80c83b57e28af6
MAC <nwokikesamson@gmail.com>
2019-08-17 07:38:56 +0100

    full ui ready

---
+++ lib/custom_icons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData menu = IconData(0xE900, fontFamily: "CustomIcons");
+  static const IconData option = IconData(0xE902, fontFamily: "CustomIcons");
+
+}
